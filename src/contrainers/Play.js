6c8738d99786d401baa72174cd5edecac4e448fc
surasee2012 <doppelganger.2011.g@gmail.com>
2019-05-02 19:46:59 +0700

    Rename Playground to Play
    - Rename Playground to Play

--- src/contrainers/Play.js
+++ src/contrainers/Play.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import CLFMonitor from '../components/playground/CLFMonitor';
-import { connect } from "react-redux";
 
 class Playground extends Component {
 
